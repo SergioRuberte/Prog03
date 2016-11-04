895dab9d349d2f7d63333837a01e3b864d8b508e
Sergio Ruberte <sergiorubertenavarro@gmail.com>
2016-11-04 19:24:49 +0100

    Ejercicio08

---
+++ Ejercicio08/src/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int num, fact=0, cont=0;
+		do{
+			System.out.println("Inserte un número mayor que cero");
+			num=teclado.nextInt();
+		}while(num<=0);
+
+	}
+
+}
