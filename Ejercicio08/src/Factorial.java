1af0e51e658456ca7dd5a058b4a78476621d1d2b
cristinaMolAg <cristi.ma88@gmail.com>
2016-11-07 18:16:11 +0100

    ejercicio8

--- Ejercicio08/src/Factorial.java
+++ Ejercicio08/src/Factorial.java
@@ -4,12 +4,13 @@ public class Factorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-		int num, fact=0, cont=0;
-		do{
-			System.out.println("Inserte un número mayor que cero");
-			num=teclado.nextInt();
-		}while(num<=0);
-
+		int num, fact=1, cont=0;
+		System.out.println("Ingrese un número:");
+		num=teclado.nextInt();
+		for (int i=num; i>0; i--){
+			fact=fact*i;
+		}
+		System.out.println("El factorial de "+num+" es: "+fact);
 	}
 
 }
